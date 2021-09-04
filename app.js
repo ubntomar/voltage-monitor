1025d19cbad78d8936246135da7b83b173455d97
Omar Hernandez Laptop <omar_alberto_h@yahoo.es>
2021-09-04 18:53:39 -0500

    Incorpore file esctructue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const {
     getMessure
-} = require('./ut.js');
+} = require('./controlers/ut.js');
 const app = express()
 const port = 3000
 //comment from aws server! this is aded from server
