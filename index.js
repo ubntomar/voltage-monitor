1bd60c4a64bf693ca9ed88fc1412086969415959
Ubuntu <ubuntu@ip-172-31-26-215.ec2.internal>
2021-09-04 02:57:46 +0000

    Express

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const {
+    getMessure
+} = require('./ut.js');
+const app = express()
+const port = 3000
+//Comment
+
+
+
+
+app.get('/', (req, res) => {
+    getMessure().then((result) => {
+        if (result)
+            res.send(JSON.stringify(result.data))
+        else res.send('Tenemos problemas, no responde la conexion a la tarjeta en el cerro')
+    })
+
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ ut.js
@@ -0,0 +1,18 @@
+const axios = require("axios")
+axios.defaults.timeout = 6000;
+const getMessure = async () => {
+    console.log("start..axios.")
+    try {
+        const response = await axios.get('http://181.60.60.57:8282/50');
+        let part = response.data.replace(/'/g, "\"")
+        let text = "{" + part + "}"
+        var res = (JSON.parse(text))
+
+    } catch (error) {
+        return false
+    }
+    return res
+}
+module.exports = {
+    getMessure
+}
