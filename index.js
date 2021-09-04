6c55bf69a01d4259b4727a101318ff3655eb1df8
Omar Hernandez Laptop <omar_alberto_h@yahoo.es>
2021-09-04 16:34:29 -0500

    message from laptop

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ const {
 } = require('./ut.js');
 const app = express()
 const port = 3000
-<<<<<<< HEAD
-//Comment
-//
 //comment from aws server! this is aded from server
-=======
->>>>>>> 0e79056037da6ae7aee33098bc77982a643411b5
-
-//from laptop
-
-
 
 app.get('/', (req, res) => {
     getMessure().then((result) => {
